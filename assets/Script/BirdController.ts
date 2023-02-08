77d86844f26fb963ae218759d8392eb9fb4919ae
JohnTran <john.tran@unicornstudio.vn>
2023-02-08 18:54:46 +0700

    add bird input controller

--- assets/Script/BirdController.ts
+++ assets/Script/BirdController.ts
@@ -1,17 +1,28 @@
-import { _decorator, Component, Node, RigidBody2D } from 'cc';
+import { _decorator, Component, Node, RigidBody2D, input, Input, EventMouse, Vec2, CCFloat } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('BirdController')
 export class BirdController extends Component {
     @property({ type: RigidBody2D })
     rb2D: RigidBody2D = null!;
+    @property({ type: CCFloat })
+    upForce: number = 200;
 
     start() {
-        console.log(this.rb2D.getMass());
+        this.rb2D.gravityScale = 3;
     }
 
-    update(deltaTime: number) {
-        
+    onLoad() {
+        input.on(Input.EventType.MOUSE_DOWN, this.addForce, this);
+    }
+    onDestroy() {
+        input.off(Input.EventType.MOUSE_DOWN, this.addForce, this);
+    }
+
+    addForce(event: EventMouse) {
+        console.log("upForce: " + this.upForce);
+        this.rb2D.linearVelocity = Vec2.ZERO;
+        this.rb2D.applyLinearImpulseToCenter(new Vec2(0, this.upForce), true);
     }
 }
 
